00de634b5d83c832c2493e4e9aaf65de7e07ebe4
JWang <1018863434@qq.com>
2022-07-15 23:49:09 +0800

    t1:helloworld

--- rabbit-mq-recv/src/main/java/cn/theten52/demo/A1Recv.java
+++ rabbit-mq-recv/src/main/java/cn/theten52/demo/A1Recv.java
@@ -9,24 +9,35 @@ import lombok.extern.slf4j.Slf4j;
 import java.nio.charset.StandardCharsets;
 
 /**
- * TODO
+ * 官网教程第一篇：HelloWorld。
+ * <p>
+ * https://www.rabbitmq.com/tutorials/tutorial-one-java.html
+ * <p>
+ * https://www.hxstrive.com/subject/rabbitmq/1051.htm
  *
  * @author wangjin
  * @date 2021/12/18
  */
 @Slf4j
-public class Recv {
-
+public class A1Recv {
+    // 队列名称
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+        // 创建连接
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+
+        //为什么我们不使用 try-with-resource 语句来自动关闭通道和连接呢？
+        // 因为这样我们的程序运行后会直接接关闭Connection并退出。
+        // 这会很尴尬，因为我们希望进程在消费者异步侦听消息到达时保持活动状态。
         Connection connection = factory.newConnection();
         log.info("connection rabbit mq success!");
 
+        // 创建通道
         Channel channel = connection.createChannel();
 
+        // 声明队列-请注意，我们也在这里声明了队列。因为我们可能在发布者之前启动消费者，所以我们要确保队列存在，然后再尝试从中消费消息。
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         log.info(" [*] Waiting for messages. To exit press CTRL+C");
 
@@ -34,6 +45,8 @@ public class Recv {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             log.info(" [x] Received '" + message + "'");
         };
+
+        //注册消费者逻辑到队列 QUEUE_NAME 上。
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
         });
     }
--- rabbit-mq-send/src/main/java/cn/theten52/demo/A1Send.java
+++ rabbit-mq-send/src/main/java/cn/theten52/demo/A1Send.java
@@ -6,23 +6,31 @@ import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * TODO
+ * 官网教程第一篇：HelloWorld。
+ * <p>
+ * https://www.rabbitmq.com/tutorials/tutorial-one-java.html
+ * <p>
+ * https://www.hxstrive.com/subject/rabbitmq/1051.htm
  *
  * @author wangjin
  * @date 2021/12/18
  */
 @Slf4j
-public class Send {
+public class A1Send {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+
+        //创建一个连接 Connection 和通道 Channel
         try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
 
+            //声明队列，名称为 QUEUE_NAME。声明一个队列是幂等的 —— 只有当它不存在时才会创建它。
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             for (int i = 0; i < 10000; i++) {
                 String message = "Hello World!";
+                //给名称为 QUEUE_NAME 的队列发送消息
                 channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
                 log.info(" [x] Sent '" + message + "'");
             }
